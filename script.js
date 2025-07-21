974192b42f7d4b0608d76befe3a5809634143194
abhishekbijuvthampi <abhishekbijuvthampi@gmail.com>
2025-07-22 02:01:39 +0530

    made images in a div and added flex properties

--- script.js
+++ script.js
@@ -1,3 +1,20 @@
+const track = document.getElementById("img-track");
+
 window.onmousedown = e => {
-    e.clientX;
+    track.dataset.mouseDownAt = e.clientX;
+}
+
+window.onmousemove = e => {
+    if(track.dataset.mouseDownAt == "0")return;
+
+    const mouseDelta = parseFloat(track.dataset.mouseDownAt) - e.clientX;
+    const maxDelta = window.innerWidth / 2 ;
+
+    const percentage = (mouseDelta/maxDelta) * -100;
+
+    track.style.transform = `translate(${percentage}%)`;
+}
+
+window.onmouseup = () => {
+    track.dataset.mouseDownAt = "0";
 }
