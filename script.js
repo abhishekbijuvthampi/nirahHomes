3a956840daf0db320a98107de8703105e24d5451
abhishekbijuvthampi <abhishekbijuvthampi@gmail.com>
2025-07-22 16:32:43 +0530

    made slideable images

--- script.js
+++ script.js
@@ -5,16 +5,24 @@ window.onmousedown = e => {
 }
 
 window.onmousemove = e => {
-    if(track.dataset.mouseDownAt == "0")return;
+    if(track.dataset.mouseDownAt == "0") return;
+
+
 
     const mouseDelta = parseFloat(track.dataset.mouseDownAt) - e.clientX;
     const maxDelta = window.innerWidth / 2 ;
 
     const percentage = (mouseDelta/maxDelta) * -100;
+    let nextPercentage = parseFloat(track.dataset.lastPercentage) + percentage;
+    
+    nextPercentage = Math.max(-270,Math.min(0,nextPercentage));
 
-    track.style.transform = `translate(${percentage}%)`;
+    track.dataset.percentage = nextPercentage;
+
+    track.style.transform = `translate(${nextPercentage}%, -50%)`;
 }
 
 window.onmouseup = () => {
     track.dataset.mouseDownAt = "0";
+    track.dataset.lastPercentage = track.dataset.percentage;
 }
